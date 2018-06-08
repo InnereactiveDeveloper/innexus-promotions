2838e54702eaa5edc9554c52d0ba7e2b59c6a6e9
Josh Glovak <glovak@innereactive.com>
2018-06-08 15:44:12 -0400

    Added Fields, Changed markup
    Added Line 1 Field
    Added Line 2 Field
    Added Extended Content Field
    Adjusted HTML output

--- promotions/fields.php
+++ promotions/fields.php
@@ -17,6 +17,7 @@ if( function_exists('acf_add_options_page') )
         'page_title'     => 'Promotions',
         'menu_title'     => 'Promotions',
         'menu_slug'     => 'promotions',
+        'icon_url' 		=> 'dashicons-calendar-alt',
         'capability'     => 'edit_posts',
         'redirect'     => false
     ));
@@ -87,11 +88,30 @@ acf_add_local_field_group(array(
 			'mime_types' => '',
 		),
 		array(
+			'key' => 'field_5a09dd5f1f1bc',
+			'label' => 'Quarter 1 Promotion Link',
+			'name' => 'q1_link',
+			'type' => 'text',
+			'instructions' => 'Where the promotion will link to.',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+		array(
 			'key' => 'field_5a09dd4b1f1bb',
-			'label' => 'Quarter 1 Promotion Content',
-			'name' => 'q1_content',
-			'type' => 'wysiwyg',
-			'instructions' => '',
+			'label' => 'Quarter 1 Promotion Line 1',
+			'name' => 'q1_line_1',
+			'type' => 'textarea',
+			'instructions' => 'Large text located on the image.',
 			'required' => 0,
 			'conditional_logic' => 0,
 			'wrapper' => array(
@@ -106,11 +126,11 @@ acf_add_local_field_group(array(
 			'delay' => 0,
 		),
 		array(
-			'key' => 'field_5a09dd5f1f1bc',
-			'label' => 'Quarter 1 Promotion Link',
-			'name' => 'q1_link',
-			'type' => 'text',
-			'instructions' => '',
+			'key' => 'field_5a09dd4b1f1bc',
+			'label' => 'Quarter 1 Promotion Line 2',
+			'name' => 'q1_line_2',
+			'type' => 'textarea',
+			'instructions' => 'Smaller text located on the image.',
 			'required' => 0,
 			'conditional_logic' => 0,
 			'wrapper' => array(
@@ -119,10 +139,29 @@ acf_add_local_field_group(array(
 				'id' => '',
 			),
 			'default_value' => '',
-			'placeholder' => '',
-			'prepend' => '',
-			'append' => '',
-			'maxlength' => '',
+			'tabs' => 'all',
+			'toolbar' => 'full',
+			'media_upload' => 1,
+			'delay' => 0,
+		),
+		array(
+			'key' => 'field_5a09dd4b1f1bd',
+			'label' => 'Quarter 1 Promotion Extended Content',
+			'name' => 'q1_extended_content',
+			'type' => 'wysiwyg',
+			'instructions' => 'Content that will appear below the promotion for extended information',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'tabs' => 'all',
+			'toolbar' => 'full',
+			'media_upload' => 1,
+			'delay' => 0,
 		),
 		array(
 			'key' => 'field_5a09ddac1f1be',
@@ -182,11 +221,30 @@ acf_add_local_field_group(array(
 			'mime_types' => '',
 		),
 		array(
-			'key' => 'field_5a09ddc81f1c0',
-			'label' => 'Quarter 2 Promotion Content',
-			'name' => 'q2_content',
-			'type' => 'wysiwyg',
-			'instructions' => '',
+			'key' => 'field_5a09dd5f2f1bc',
+			'label' => 'Quarter 2 Promotion Link',
+			'name' => 'q2_link',
+			'type' => 'text',
+			'instructions' => 'Where the promotion will link to.',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+		array(
+			'key' => 'field_5a09dd4b2f1bb',
+			'label' => 'Quarter 2 Promotion Line 1',
+			'name' => 'q2_line_1',
+			'type' => 'textarea',
+			'instructions' => 'Large text located on the image.',
 			'required' => 0,
 			'conditional_logic' => 0,
 			'wrapper' => array(
@@ -201,11 +259,11 @@ acf_add_local_field_group(array(
 			'delay' => 0,
 		),
 		array(
-			'key' => 'field_5a09ddd71f1c1',
-			'label' => 'Quarter 2 Promotion Link',
-			'name' => 'q2_link',
-			'type' => 'text',
-			'instructions' => '',
+			'key' => 'field_5a09dd4b2f1bc',
+			'label' => 'Quarter 2 Promotion Line 2',
+			'name' => 'q2_line_2',
+			'type' => 'textarea',
+			'instructions' => 'Smaller text located on the image.',
 			'required' => 0,
 			'conditional_logic' => 0,
 			'wrapper' => array(
@@ -214,10 +272,29 @@ acf_add_local_field_group(array(
 				'id' => '',
 			),
 			'default_value' => '',
-			'placeholder' => '',
-			'prepend' => '',
-			'append' => '',
-			'maxlength' => '',
+			'tabs' => 'all',
+			'toolbar' => 'full',
+			'media_upload' => 1,
+			'delay' => 0,
+		),
+		array(
+			'key' => 'field_5a09dd4b2f1bd',
+			'label' => 'Quarter 2 Promotion Extended Content',
+			'name' => 'q2_extended_content',
+			'type' => 'wysiwyg',
+			'instructions' => 'Content that will appear below the promotion for extended information',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'tabs' => 'all',
+			'toolbar' => 'full',
+			'media_upload' => 1,
+			'delay' => 0,
 		),
 		array(
 			'key' => 'field_5a09df791f1c2',
@@ -277,11 +354,30 @@ acf_add_local_field_group(array(
 			'mime_types' => '',
 		),
 		array(
-			'key' => 'field_5a09df911f1c5',
-			'label' => 'Quarter 3 Promotion Content',
-			'name' => 'q3_content',
-			'type' => 'wysiwyg',
-			'instructions' => '',
+			'key' => 'field_5a09dd5f3f1bc',
+			'label' => 'Quarter 3 Promotion Link',
+			'name' => 'q3_link',
+			'type' => 'text',
+			'instructions' => 'Where the promotion will link to.',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+		array(
+			'key' => 'field_5a09dd4b3f1bb',
+			'label' => 'Quarter 3 Promotion Line 1',
+			'name' => 'q3_line_1',
+			'type' => 'textarea',
+			'instructions' => 'Large text located on the image.',
 			'required' => 0,
 			'conditional_logic' => 0,
 			'wrapper' => array(
@@ -296,11 +392,11 @@ acf_add_local_field_group(array(
 			'delay' => 0,
 		),
 		array(
-			'key' => 'field_5a09df9c1f1c6',
-			'label' => 'Quarter 3 Promotion Link',
-			'name' => 'q3_link',
-			'type' => 'text',
-			'instructions' => '',
+			'key' => 'field_5a09dd4b3f1bc',
+			'label' => 'Quarter 3 Promotion Line 2',
+			'name' => 'q3_line_2',
+			'type' => 'textarea',
+			'instructions' => 'Smaller text located on the image.',
 			'required' => 0,
 			'conditional_logic' => 0,
 			'wrapper' => array(
@@ -309,10 +405,29 @@ acf_add_local_field_group(array(
 				'id' => '',
 			),
 			'default_value' => '',
-			'placeholder' => '',
-			'prepend' => '',
-			'append' => '',
-			'maxlength' => '',
+			'tabs' => 'all',
+			'toolbar' => 'full',
+			'media_upload' => 1,
+			'delay' => 0,
+		),
+		array(
+			'key' => 'field_5a09dd4b3f1bd',
+			'label' => 'Quarter 3 Promotion Extended Content',
+			'name' => 'q3_extended_content',
+			'type' => 'wysiwyg',
+			'instructions' => 'Content that will appear below the promotion for extended information',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'tabs' => 'all',
+			'toolbar' => 'full',
+			'media_upload' => 1,
+			'delay' => 0,
 		),
 		array(
 			'key' => 'field_5a09df821f1c3',
@@ -372,11 +487,30 @@ acf_add_local_field_group(array(
 			'mime_types' => '',
 		),
 		array(
-			'key' => 'field_5a09dfbb1f1c8',
-			'label' => 'Quarter 4 Promotion Content',
-			'name' => 'q4_content',
-			'type' => 'wysiwyg',
-			'instructions' => '',
+			'key' => 'field_5a09dd5f4f1bc',
+			'label' => 'Quarter 4 Promotion Link',
+			'name' => 'q4_link',
+			'type' => 'text',
+			'instructions' => 'Where the promotion will link to.',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+		array(
+			'key' => 'field_5a09dd4b4f1bb',
+			'label' => 'Quarter 4 Promotion Line 1',
+			'name' => 'q4_line_1',
+			'type' => 'textarea',
+			'instructions' => 'Large text located on the image.',
 			'required' => 0,
 			'conditional_logic' => 0,
 			'wrapper' => array(
@@ -391,11 +525,11 @@ acf_add_local_field_group(array(
 			'delay' => 0,
 		),
 		array(
-			'key' => 'field_5a09dfc41f1c9',
-			'label' => 'Quarter 4 Promotion Link',
-			'name' => 'q4_link',
-			'type' => 'text',
-			'instructions' => '',
+			'key' => 'field_5a09dd4b4f1bc',
+			'label' => 'Quarter 4 Promotion Line 2',
+			'name' => 'q4_line_2',
+			'type' => 'textarea',
+			'instructions' => 'Smaller text located on the image.',
 			'required' => 0,
 			'conditional_logic' => 0,
 			'wrapper' => array(
@@ -404,10 +538,29 @@ acf_add_local_field_group(array(
 				'id' => '',
 			),
 			'default_value' => '',
-			'placeholder' => '',
-			'prepend' => '',
-			'append' => '',
-			'maxlength' => '',
+			'tabs' => 'all',
+			'toolbar' => 'full',
+			'media_upload' => 1,
+			'delay' => 0,
+		),
+		array(
+			'key' => 'field_5a09dd4b4f1bd',
+			'label' => 'Quarter 4 Promotion Extended Content',
+			'name' => 'q4_extended_content',
+			'type' => 'wysiwyg',
+			'instructions' => 'Content that will appear below the promotion for extended information',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'tabs' => 'all',
+			'toolbar' => 'full',
+			'media_upload' => 1,
+			'delay' => 0,
 		),
 	),
 	'location' => array(
@@ -431,6 +584,7 @@ acf_add_local_field_group(array(
 
 };
 
+//Shortcode
 if( function_exists('acf_add_local_field_group') )
 {
 	function promotions_func( $atts, $content = "" ) 
@@ -472,19 +626,31 @@ if( function_exists('acf_add_local_field_group') )
 		
 		$image 		= get_field($q . '_image', 'option');
 		$image 		= $image['sizes']['large'];
-		$content 	= get_field($q . '_content', 'option');
+		$line1 		= get_field($q . '_line_1', 'option');
+		$line2 		= get_field($q . '_line_2', 'option');
+		$content 	= get_field($q . '_extended_content', 'option');
 		$link 		= get_field($q . '_link', 'option');
 	
-		echo '<div class="promotion snippet '.$type.'">';
-			echo '<a href="'.$link.'">';
+		echo '<div class="innexus-promotion-wrapper '.$type.'">';
 			
-				echo '<div class="promotion-container">';
-					echo '<img src=" '.$image.'" title="'.$content.'"/>';
-					echo '<div class="promotion-content">';
-						echo $content;
+			//Wrap the full promotion in a link
+			echo '<a href="'.$link.'">';
+				echo '<div class="innexus-promotion-container">';
+					
+					//Keep the lines wrapped so we can easily shift the content around
+					echo '<div class="innexus-promotion-line-container">';
+						echo '<div class="innexus-promotion-line-1">'.$line1.'</div>';
+						echo '<div class="innexus-promotion-line-2">'.$line2.'</div>';
 					echo '</div>';
+					
+					echo '<img src=" '.$image.'" title="'.$line1.' alt="'.$line1.'"/>';
 				echo '</div>';
 			echo '</a>';
+			
+			//Keep extended content below the promotion
+			echo '<div class="innexus-promotion-extended-content">';
+				echo $content;
+			echo '</div>';
 		echo '</div>';
 	}
 	
